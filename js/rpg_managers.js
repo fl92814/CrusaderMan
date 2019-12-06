fc1fc21a031ee8e0bb25ae9e85cc6b79ec34ec5d
Derek Drost <rlwarrior32@hotmail.com>
2019-12-05 22:44:34 -0600

    Reverted database changes

--- js/rpg_managers.js
+++ js/rpg_managers.js
@@ -40,7 +40,6 @@ var $gameTroop        = null;
 var $gameMap          = null;
 var $gamePlayer       = null;
 var $testEvent        = null;
-var $actionKeywords   = null;
 
 DataManager._globalId       = 'RPGMV';
 DataManager._lastAccessedId = 1;
@@ -60,8 +59,7 @@ DataManager._databaseFiles = [
     { name: '$dataTilesets',     src: 'Tilesets.json'     },
     { name: '$dataCommonEvents', src: 'CommonEvents.json' },
     { name: '$dataSystem',       src: 'System.json'       },
-    { name: '$dataMapInfos',     src: 'MapInfos.json'     },
-    { name: '$dataActionKeywords',    src: 'ActionKeywords.json'     }
+    { name: '$dataMapInfos',     src: 'MapInfos.json'     }
 ];
 
 DataManager.loadDatabase = function() {
